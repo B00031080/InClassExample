4d3e528908be6814b5042b592324f2b5f5c82b59
Christophe Harrer <B00031080@essec.edu>
2020-06-09 11:52:18 +0200

    DataBase Integration is Done
    Don't forget to use a secrets.ini file.

--- src/routes/garden.svelte
+++ src/routes/garden.svelte
@@ -1,1 +1,90 @@
-<h1>This is going to be the map of my garden</h1>
+
+<script context="module">
+	export async function preload(page, session) {
+
+		const res = await this.fetch(`/vegetableslist`);
+		const vegetables = await res.json();
+
+		return { vegetables };
+	}
+</script>
+
+<script>
+    /*let vegetables = [{
+        "name":"Peas",
+        "description":"Peas are very tasty when well cooked.",
+        "picture":"/peas.jpg",
+        "picalt":"Showing peas"
+    },
+    {
+        "name":"Tomato",
+        "description":"Delicious on pizza.",
+        "picture":"/tomato.jpg",
+        "picalt":"Showing tomato plant"
+    },
+    {
+        "name":"Patato",
+        "description":"Great for French fries.",
+        "picture":"/patato.jpg",
+        "picalt":"Showing patato plant"
+    }];*/
+    export let vegetables;
+    let newName;
+    let newDescription;
+
+    async function createNewVeg(){
+        const res = await fetch(`/newveg`,{
+            method:"POST",
+            body:JSON.stringify({
+                name:newName,
+                description : newDescription
+            })
+        });
+        vegetables = await res.json();
+        newName = "";
+        newDescription = "";
+    }
+
+    function deleteMe(id){
+        return async function(){
+            const res = await fetch(`/deleteveg/`+ id ,{
+            method:"DELETE"
+        });
+        vegetables = await res.json();
+        }
+    }
+</script>
+
+<style>
+    
+</style>
+
+
+
+<h1 class="text-3xl text-green-800 font-black font-sans uppercase leading-loose">Vegetables you can plant</h1>
+<div class="space-y-3"> 
+    {#each vegetables as veg}
+        <div class="flex border-green-300 border-2 rounded-lg p-3 space-x-3">
+            <div class="w-1/4">
+                <img class="rounded-lg" src={veg.picture} alt={veg.picalt}/> 
+            </div>
+            <div class="w-3/4">
+                <h3 class="uppercase text-lg font-bold text-green-600">{veg.name}</h3>
+                <p>{veg.description}.</p>
+                <div on:click={deleteMe(veg._id)}>Delete</div>
+            </div>
+        </div> 
+    {/each}
+    <div class="flex border-green-300 border-2 rounded-lg p-3 space-x-3">
+            <div class="w-1/4">
+                <img class="rounded-lg" src="question.png" alt="Add a veg"/> 
+            </div>
+            <div class="w-3/4">
+                <form on:submit|preventDefault={createNewVeg}>
+                    <div><span>Name :</span><input type="text" bind:value={newName}/></div>
+                    <div><span>Description : </span><input type="text" bind:value={newDescription}/></div>
+                    <input type="submit"/>
+                </form>
+            </div>
+    </div> 
+</div>
--- src/server.js
+++ src/server.js
@@ -1,42 +1,105 @@
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const fs = require('fs');
 const ini = require('ini');
-console.log(__dirname+'/../../../secrets.ini');
-const config = ini.parse(fs.readFileSync(__dirname+'/../../../secrets.ini', 'utf-8'));
+console.log(__dirname + '/../../../secrets.ini');
+const config = ini.parse(fs.readFileSync(__dirname + '/../../../secrets.ini', 'utf-8'));
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
+const app = express();
+app.use(express.json({
+	type: ['application/json', 'text/plain']
+}));
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(cors());
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
 
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 var BSONRegExp = mongodb.BSONRegExp;
-const uri = "mongodb+srv://" + config.username + ":" + config.password + "@"+config.dburl+".mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://" + config.username + ":" + config.password + "@" + config.dburl + ".mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-	if(err)console.log("Connection failed du to :", err);
 	var db = client.db("garden");
-    var collection = db.collection("plants");
-    
-    var query = {
-        "color": "green"
-    };
-    
-    collection.find(query).toArray((err,docs)=>{
-		console.log(err,docs);
-		client.close();
+	var collection = db.collection("plants");
+
+	app.get("/vegetableslist", (req, res) => {
+		collection.find({}).toArray((err, docs) => {
+			console.log(err, docs);
+			res.send(docs);
+		});
+	});
+
+	app.post("/newveg", (req, res) => {
+		console.log(req.body);
+
+		collection.insertOne({
+			name: req.body.name,
+			description: req.body.description,
+			picture: "/question.png",
+			picalt: "Waiting for a real pic."
+		}, function (err, resmongo) {
+			collection.find({}).toArray((err, docs) => {
+				console.log(err, docs);
+				res.send(docs);
+			});
+		});
 	});
 	
+	app.post("/editveg/:id", (req, res) => {
+		console.log(req.body);
+
+		collection.updateOne({_id:mongodb.ObjectId(req.params.id)},{
+			name: req.body.name,
+			description: req.body.description,
+			picture: req.body.picture,
+			picalt: req.body.picalt
+		}, function (err, resmongo) {
+			collection.find({}).toArray((err, docs) => {
+				console.log(err, docs);
+				res.send(docs);
+			});
+		});
+	});
+
+	app.delete("/deleteveg/:id", (req, res) => {
+		console.log(req.params.id);
+		collection.deleteOne( {_id:mongodb.ObjectId(req.params.id)}, function (err, obj) {
+			collection.find({}).toArray((err, docs) => {
+				res.send(docs);
+			});
+		});
+	})
+
+	app.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware()
+	);
+
+
+
+	app.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
+
+
+	/*if (err) console.log("Connection failed du to :", err);
+	
+
+	var query = {
+		"color": "green"
+	};
+
+	collection.find(query).toArray((err, docs) => {
+		console.log(err, docs);
+		client.close();
+	});*/
+
 });
